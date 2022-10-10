b7fcb53218f623839386d51834cf3142ebfa1191
Cole-T-Harris <cole.t.harr@gmail.com>
2022-10-09 19:33:24 -0600

    Re-entering version 0.0 of personal website files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Listen to scroll to change header opacity class
  */
 function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+    var startY = $('.navbar').height() * 1.5; //The point where the navbar changes in px
 
     if($(window).scrollTop() > startY){
         $('.navbar').addClass("scrolled");
@@ -14,8 +14,32 @@ function checkScroll(){
     }
 }
 
+function adjustAboutBio() {
+    //Adjust about paragraph classes based on window width
+    if (window.innerWidth < 840) {
+        $('#about-bio').removeClass("col-8");
+        $('#about-bio').addClass("col");
+    }
+    else {
+        $('#about-bio').addClass("col-8");
+        $('#about-bio').removeClass("col");
+    }
+}
+
+// Add active class if in that section of website
+function addActive() {
+    $('body').scrollspy({
+        target: '#navigationBar'
+    });
+}
+
 if($('.navbar').length > 0){
     $(window).on("scroll load resize", function(){
         checkScroll();
+        adjustAboutBio();
+        addActive();
     });
 }
+
+
+
